991cdacea83bf7292d5ebba84591e3c7e79d0849
Alimusim <alimossim.agayev@fdrive.kz>
2024-10-05 13:32:01 +0500

    resolve rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import { FILES_FOLDER } from '../constants.js';
+import { promises as fs} from 'fs';
+import path from 'path';
 const rename = async () => {
-    // Write your code here 
+    const currentPath = path.resolve(FILES_FOLDER, 'wrongFilename.txt');
+    const newPath = path.resolve(FILES_FOLDER, 'properFilename.md');
+    try {
+        await fs.access(currentPath, constants.F_OK)
+    } catch (error) {
+        if(error.code === 'ENOENT') {
+            throw new Error('FS operation failed')
+        }
+    }
+    try {
+        await fs.access(newPath, constants.F_OK)
+        throw new Error('FS operation failed')
+    } catch (error) {
+        if(error.code === 'ENOENT') {
+            await fs.rename(currentPath,newPath)
+        }
+       
+    }
+    
 };
 
 await rename();

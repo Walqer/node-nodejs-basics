b3e1946ec812548abec901267d263af0c23c8dae
Alimusim <alimossim.agayev@fdrive.kz>
2024-10-06 02:11:26 +0500

    resolve args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+   const args = process.argv.slice(2)
+   const result = []
+   for(let i = 0; i < args.length; i++) {
+    const arg = args[i].slice(2);
+    const value = args[i+1]
+    result.push(`${arg} is ${value}`)
+    i++
+   }
+   console.log(result.join(', '));
 };
 
 parseArgs();

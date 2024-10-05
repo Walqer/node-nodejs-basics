c268cc770d08f5cb642911b682f6878bb2fa464b
Alimusim <alimossim.agayev@fdrive.kz>
2024-10-05 22:32:08 +0500

    feat: add remove method

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { FILES_FOLDER } from '../constants.js';
+import { promises as fs} from 'fs';
+import path from 'path';
 const remove = async () => {
-    // Write your code here 
+    const fileToRemove = path.resolve(FILES_FOLDER, 'fileToRemove.txt');
+    try{
+        fs.unlink(fileToRemove);
+    }catch(error){
+        if(error.code === 'ENOENT') {
+            throw new Error('FS operation failed')
+        }
+    }
 };
 
 await remove();

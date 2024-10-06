90f687cbe5e0e50b6d0fd0bc44353724b5bf27a6
Alimusim <alimossim.agayev@fdrive.kz>
2024-10-06 14:00:12 +0500

    resolve hash

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const FILES_FOLDER = 'src/fs/files';
 export const MODULES_FOLDER = 'src/modules';
+export const HASH_FOLDER = 'src/hash/files';
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import crypto from 'crypto'
+import { createReadStream} from 'fs';
+import {HASH_FOLDER} from '../constants.js'
+import path from 'path'
 const calculateHash = async () => {
-    // Write your code here 
+   const pathToFile = path.resolve(path.join(HASH_FOLDER, 'fileToCalculateHashFor.txt'));
+   const fileStream = createReadStream(pathToFile);
+   const hash = crypto.createHash('sha256');
+   fileStream.on('data',(chunk) => {
+    hash.update(chunk);
+   })
+   fileStream.on('end', () => {
+    const result = hash.digest('hex');
+    console.log(`SHA256 hash for file: ${result}`);
+});
 };
 
-await calculateHash();
+await calculateHash();  

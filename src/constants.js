69666a1b71588fc11f2c607b4edbc6eb5dfc7a0d
Alimusim <alimossim.agayev@fdrive.kz>
2024-10-06 16:34:45 +0500

    solve  read js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const FILES_FOLDER = 'src/fs/files';
 export const MODULES_FOLDER = 'src/modules';
 export const HASH_FOLDER = 'src/hash/files';
+export const STREAM_FOLDER = 'src/streams/files';

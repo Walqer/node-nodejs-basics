8efeaf9faa12b07e1009b46c08aea10a14493fd7
Alimusim <alimossim.agayev@fdrive.kz>
2024-10-06 18:23:41 +0500

    resolve compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import path from 'path'
+import { ZIP_FOLDER } from '../constants.js';
+import fs from 'fs'
+import  zlib  from 'zlib'
+
+
 const compress = async () => {
-    // Write your code here 
+    const pathToFile = path.resolve(path.join(ZIP_FOLDER, 'fileToCompress.txt'));
+    const pathToZipFile = path.resolve(path.join(ZIP_FOLDER, 'farchive.gz'));
+    const inputFile = fs.createReadStream(pathToFile);
+    const outputFile = fs.createWriteStream(pathToZipFile);
+    outputFile.on('finish', () => {
+        console.log('File successfully compressed to', pathToZipFile);
+    })
+    const gzip = zlib.createGzip();
+    inputFile.pipe(gzip).pipe(outputFile);
 };
 
 await compress();

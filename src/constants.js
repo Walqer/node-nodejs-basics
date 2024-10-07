9a647de8a52f3b20957e7ddb78d0f24f684174d1
Alimusim <alimossim.agayev@fdrive.kz>
2024-10-07 20:18:29 +0500

    resolve child proccess

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,4 @@ export const HASH_FOLDER = 'src/hash/files';
 export const STREAM_FOLDER = 'src/streams/files';
 export const ZIP_FOLDER = 'src/zip/files';
 export const WT_FOLDER = 'src/wt'
+export const CP_FOLDER = 'src/cp/files'
